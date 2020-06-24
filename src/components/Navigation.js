592310ccb22caed59f1f8dfadc8fb92cbdf98a4a
Angelika Villanueva <angelika@Angelikas-MacBook-Pro.local>
2020-06-24 17:59:59 +0800

    add props to li

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Li = styled.li `
-    border-bottom: 4px solid #4CA7BC;
+    border-bottom: ${props => props.active ? "4px solid rgb(76, 167, 188, 50%)" : "4px solid rgb(76, 167, 188, 100%)"};
 `
 const A = styled.a `
     color: #0D3843;
     font-family: 'Montserrat', sans-serif;
+    opacity: 1;
     &:hover {
         color: #0D3843;
         opacity: .7; 
@@ -16,7 +17,7 @@ const A = styled.a `
 function Navigation() {
     return (<nav className="navbar navbar-expand m-5">
         <ul className="navbar-nav mr-auto">
-            <Li className="nav-item px-2">
+            <Li active className="nav-item px-2">
                 <A className="nav-link" href="Home.js">HOME</A>
             </Li>
             <Li className="nav-item px-2">
