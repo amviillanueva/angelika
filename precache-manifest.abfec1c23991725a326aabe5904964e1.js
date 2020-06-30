888df69ddb9a0b1699492f738bc100f74791eae4
Angelika Villanueva <angelika@Angelikas-MacBook-Pro.local>
2020-06-30 23:10:03 +0800

    Updates

---
+++ precache-manifest.abfec1c23991725a326aabe5904964e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a962433e1558df48bb567c22303cb6",
+    "url": "/angelika/index.html"
+  },
+  {
+    "revision": "207644a4aa7ef58ce4f5",
+    "url": "/angelika/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "207644a4aa7ef58ce4f5",
+    "url": "/angelika/static/js/2.59927677.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/angelika/static/js/2.59927677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73ac96179792dd10460",
+    "url": "/angelika/static/js/main.54d627e6.chunk.js"
+  },
+  {
+    "revision": "a1dc123575041f1993ed",
+    "url": "/angelika/static/js/runtime-main.fbd6a7b8.js"
+  },
+  {
+    "revision": "c93a7c8ec3cdc55fde7d69fbd7f71c3c",
+    "url": "/angelika/static/media/profile.c93a7c8e.png"
+  }
+]);
