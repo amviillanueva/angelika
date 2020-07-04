6ead63d8ee78135357113451f6686cb952a7e95e
Angelika Villanueva <angelika@Angelikas-MacBook-Pro.local>
2020-07-04 15:17:55 +0800

    change logo

---
+++ src/components/info/eduList.js
@@ -0,0 +1,15 @@
+const eduList = [
+    {
+        school: "Tertiary: Polytechnic University of the Philippines - Sta. Mesa, Manila",
+        year: "(2018-Present)",
+        description: "Course: Diploma in Information Communication Technology"
+    },
+    {
+        school: "Secondary: Sumulong College of Arts and Sciences - Antipolo City",
+        year: "(2016-2018)",
+        description: "Track: TVL-ICT",
+        research: "Research: Grading System for Senior High School Students in SCAS"
+    }
+]
+
+export default eduList;
